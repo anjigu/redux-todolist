21b92481b2693341ca8c38014ecf47db7cabd51d
anjisu <guajs0818@gmail.com>
2022-09-13 01:47:19 +0900

    리덕스 툴킷으로 투두리스트 만들기

---
+++ src/ToDo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux"
+import { remove } from "./store";
+
+function ToDo({ text, onBtnClick }) {
+  return (
+    <li>
+      {text} <button onClick={onBtnClick}>DEL</button>
+    </li>
+  );
+};
+
+
+function mapDispatchToProps(dispatch, ownProps){
+  return {
+    onBtnClick: () => dispatch(remove(ownProps.id))
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
